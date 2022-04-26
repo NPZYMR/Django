345c368ace8c03594f856fde20f06892fe29ee68
Naveen Pandia <naveen.pandia@zymr.com>
2022-04-26 18:21:35 +0530

    till Login & Logout

--- myapp/views.py
+++ myapp/views.py
@@ -103,3 +103,11 @@ def login(request):
     else:
         return render(request,'login.html')
 
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
